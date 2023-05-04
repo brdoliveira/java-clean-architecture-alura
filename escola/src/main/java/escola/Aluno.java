4bb5513017c6b44cb44e2f4d9a41e65aef27996f
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-05-03 23:46:27 -0300

    FEAT(Aluno): Adicionando construtor

--- escola/src/main/java/escola/Aluno.java
+++ escola/src/main/java/escola/Aluno.java
@@ -11,9 +11,31 @@ public class Aluno {
 
     private List<Telefone> telefones = new ArrayList<>();
 
+    public Aluno(CPF cpf, String nome, Email email) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.email = email;
+    }
+
     public void adicionarTelefone(String ddd, String nome){
         this.telefones.add(
                 new Telefone(ddd,nome)
         );
+    }
+
+    public CPF getCpf() {
+        return cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Email getEmail() {
+        return email;
+    }
+
+    public List<Telefone> getTelefones() {
+        return telefones;
     }
 }
