45b7437805dfa72a7e3009ada8b8e8b99517f129
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-05-08 23:56:44 -0300

    FEAT(EnviarEmailIndicacaoComJavaMail): Criando interface EnviarEmailIndicacaoComJavaMail

---
+++ escola/src/main/java/escola/infra/indicacao/EnviarEmailIndicacaoComJavaMail.java
@@ -0,0 +1,13 @@
+package escola.infra.indicacao;
+
+
+import escola.aplicacao.indicacao.EnviarEmailIndicacao;
+import escola.dominio.aluno.Aluno;
+
+public class EnviarEmailIndicacaoComJavaMail implements EnviarEmailIndicacao {
+
+	@Override
+	public void enviarPara(Aluno indicado) {
+		// logica de envio de email com a lib Java Mail
+	}
+}
