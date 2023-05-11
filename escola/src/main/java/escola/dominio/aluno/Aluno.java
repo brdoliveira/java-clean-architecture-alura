9752875fab336f93efc10d1f7cbdd6f0404bb671
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-05-10 23:56:36 -0300

    FEAT(Aluno): Adicionando validação do telefone

--- escola/src/main/java/escola/dominio/aluno/Aluno.java
+++ escola/src/main/java/escola/dominio/aluno/Aluno.java
@@ -20,6 +20,9 @@ public class Aluno {
     }
 
     public void adicionarTelefone(String ddd, String nome){
+        if(telefones.size() == 2){
+            throw new IllegalArgumentException(("Numero maximo de telefone atingido!"));
+        }
         this.telefones.add(
                 new Telefone(ddd,nome)
         );
