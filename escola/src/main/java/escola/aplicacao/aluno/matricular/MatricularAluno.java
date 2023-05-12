238fb0f53966312aac1b21e052806961d3f56174
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-05-12 20:39:38 -0300

    FEAT(MtricularAluno): Alterando método executa()

--- escola/src/main/java/escola/aplicacao/aluno/matricular/MatricularAluno.java
+++ escola/src/main/java/escola/aplicacao/aluno/matricular/MatricularAluno.java
@@ -1,17 +1,25 @@
 package escola.aplicacao.aluno.matricular;
 
+import escola.dominio.PublicadorDeEventos;
 import escola.dominio.aluno.Aluno;
+import escola.dominio.aluno.AlunoMatriculado;
 import escola.dominio.aluno.RepositorioDeAlunos;
 
 public class MatricularAluno {
     private final RepositorioDeAlunos repositorio;
+    private final PublicadorDeEventos publicador;
 
-    public MatricularAluno(RepositorioDeAlunos repositorio){
+
+    public MatricularAluno(RepositorioDeAlunos repositorio, PublicadorDeEventos publicador){
         this.repositorio = repositorio;
+        this.publicador = publicador;
     }
 
     public void executa(MatricularAlunoDTO dados){
         Aluno alunoNovo = dados.criarAluno();
         repositorio.matricular(alunoNovo);
+
+        AlunoMatriculado evento = new AlunoMatriculado(alunoNovo.getCpf());
+        publicador.publicar(evento);
     }
 }
