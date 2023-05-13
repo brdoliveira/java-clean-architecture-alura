f8b73eddbcc65d1410d9420b1a3be51cbcf92d90
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-05-13 19:53:02 -0300

    FEAT(RepositorioDeSelos): Criando interface RepositorioDeSelos()

---
+++ escola/src/main/java/escola/dominio/selo/RepositorioDeSelos.java
@@ -0,0 +1,11 @@
+package escola.dominio.selo;
+
+import escola.dominio.aluno.CPF;
+
+import java.util.List;
+
+public interface RepositorioDeSelos {
+
+    void adicionar(Selo selo);
+    List<Selo> selosDoAlunoDeCPF(CPF cpf);
+}
